d4498ec640988996663221395ee434d16db9add0
40213151 <daimaruplay@gmail.com>
2018-10-15 14:48:05 +0900

    aaaa

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,7 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
