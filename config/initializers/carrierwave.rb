a28b946edbd08dfa20836dbf73ff976ead961fdd
40213151 <daimaruplay@gmail.com>
2018-10-15 15:02:15 +0900

    aaaa

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'chatspace2-test'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace2-test'
+  config.fog_directory  = 'upload-test01'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-test01'
 end
