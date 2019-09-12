c51fda5d16c9afc0d9d34bf287f0d3670c0a6573
40213151 <daimaruplay@gmail.com>
2019-09-12 15:25:00 +0900

    aaa

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.1'
 
 # Capistranoのログの表示に利用する
-set :application, '自身のアプリケーション名'
+set :application, 'chatspace2'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:<Githubのユーザー名>/<レポジトリ名>.git'
